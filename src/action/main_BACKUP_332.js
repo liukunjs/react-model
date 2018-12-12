1728829bf54f8cfb8576c9628840e4b28e1130fa
kunliu <liukunjs@sina.com>
2018-12-12 11:13:22 +0800

    asfdsa

--- src/action/main_BACKUP_332.js
+++
@@ -1,6 +0,0 @@
-function secend () {
-	console.log("sendec")
-}
-export a = function (){
-	console.log(898989)
-}
--- src/action/main_LOCAL_12208.js
+++
@@ -1,6 +0,0 @@
-function secend () {
-	console.log("sendec")
-}
-export function (){
-	console.log(898989)
-}
